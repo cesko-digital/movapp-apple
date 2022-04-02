d1749eefcca8e7710f583f76207969d8d17b8b1d
Martin Kluska <martin@kluska.cz>
2022-04-02 13:22:06 +0200

    Initial Commit

---
+++ Shared/MovappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovappApp.swift
+//  Shared
+//
+//  Created by Martin Kluska on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

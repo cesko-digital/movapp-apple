ae0bece241e8e762e4752afa89d2f64e33dd110a
Darina Melis <darina05@email.cz>
2022-04-11 13:19:36 +0200

    WIP: ForChildrenItem view added

--- Shared/ForChildren/ForChildrenItem.swift
+++ Shared/ForChildren/ForChildrenItem.swift
@@ -3,13 +3,83 @@
 //  Movapp (iOS)
 //
 //  Created by Daryna Polevyk on 11.04.2022.
-//
 
 import SwiftUI
 
 struct ForChildrenItem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            VStack (spacing: 20) {
+                Image("auticka")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.black)
+                VStack {
+                    HStack {
+                        Image("flagCzech")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.8), radius: 38, x: 0, y: 19)
+                        Text("česky")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Slova")
+                                .foregroundColor(Color("colors/text"))
+                            Text("Tanskripce")
+                                .foregroundColor(Color("colors/secondary"))
+                        }
+                        Spacer()
+                        Image(systemName: "play.circle") //(systemName: soundService.isPlaying ? "stop.circle" : "play.circle")
+                            .resizable()
+                            .foregroundColor(Color("colors/action"))
+                            .frame(width: 30, height: 30)
+                            .onTapGesture {
+                                // soundService.speach(language: language, text: example.example)
+                            }
+                    }
+                    HStack {
+                        Image("flagUkraine")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.8), radius: 38, x: 0, y: 19)
+                        Text("ukrajinsky")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Slova")
+                                .foregroundColor(Color("colors/text"))
+                            Text("Tanskripce")
+                                .foregroundColor(Color("colors/secondary"))
+                        }
+                        Spacer()
+                        Image(systemName: "play.circle") //(systemName: soundService.isPlaying ? "stop.circle" : "play.circle")
+                            .resizable()
+                            .foregroundColor(Color("colors/action"))
+                            .frame(width: 30, height: 30)
+                            .onTapGesture {
+                                // soundService.speach(language: language, text: example.example)
+                            }
+                    }
+                }
+                .padding(10)
+                .frame(maxWidth: .infinity)
+                .background(Color("colors/yellow"))
+                
+            }
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding()
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.04), radius: 38, x: 0, y: 19)
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.04), radius: 12, x: 0, y: 15)
     }
 }
 
@@ -18,3 +88,4 @@ struct ForChildrenItem_Previews: PreviewProvider {
         ForChildrenItem()
     }
 }
+

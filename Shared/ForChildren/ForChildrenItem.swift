ef9b8fed3c0a90734045e4ce439242b9b7e23893
Daryna Polevyk <daryna.polevyk@quanti.cz>
2022-04-11 13:08:59 +0200

    WIP: ForChildrenItem file added

---
+++ Shared/ForChildren/ForChildrenItem.swift
@@ -0,0 +1,20 @@
+//
+//  ForChildrenItem.swift
+//  Movapp (iOS)
+//
+//  Created by Daryna Polevyk on 11.04.2022.
+//
+
+import SwiftUI
+
+struct ForChildrenItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForChildrenItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ForChildrenItem()
+    }
+}

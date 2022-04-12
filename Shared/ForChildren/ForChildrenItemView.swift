54d996d6f4bbac659f79373438f578c042ccc1e0
Martin Kluska <martin@kluska.cz>
2022-04-12 21:47:49 +0200

    Refactored for children item view to card view + remove view definition duplication + play sound

--- Shared/ForChildren/ForChildrenItemView.swift
+++ Shared/ForChildren/ForChildrenItemView.swift
@@ -6,22 +6,6 @@
 
 import SwiftUI
 
-private extension Image {
-    func flagStyle () -> some View {
-        self.resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 30)
-            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.8), radius: 38, x: 0, y: 19)
-    }
-}
-
-
-private extension Text {
-    func languageStyle () -> some View {
-        self.font(.system(size: 20))
-            .fontWeight(.bold)
-    }
-}
 
 
 struct ForChildrenItemView: View {
@@ -29,81 +13,32 @@ struct ForChildrenItemView: View {
     
     var body: some View {
         
-        VStack {
-            // Ensure that the image is not stretched to wide and to big
-            Image("for-children/\(item.imageName)")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxHeight: 300)
-                .padding(20)
-            
-            VStack {
-                HStack {
-                    Image("icons/flags/czech")
-                        .flagStyle()
-                    
-                    Text("česky")
-                        .languageStyle()
-                    
-                    Spacer()
-                }
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(item.translationFrom)
-                            .foregroundColor(Color("colors/text"))
-                        Text("[\(item.transcriptionFrom)]")
-                            .foregroundColor(Color("colors/secondary"))
-                    }
-                    Spacer()
-                    Image(systemName: "play.circle") //(systemName: soundService.isPlaying ? "stop.circle" : "play.circle")
-                        .resizable()
-                        .foregroundColor(Color("colors/action"))
-                        .frame(width: 30, height: 30)
-                        .onTapGesture {
-                            // soundService.speach(language: language, text: example.example)
-                        }
-                }
-                HStack {
-                    Image("icons/flags/ukraine")
-                        .flagStyle()
-                    
-                    Text("ukrajinsky")
-                        .languageStyle()
-                    Spacer()
-                }
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(item.translationTo)
-                            .foregroundColor(Color("colors/text"))
-                        Text("[\(item.transcriptionTo)]")
-                            .foregroundColor(Color("colors/secondary"))
-                    }
-                    Spacer()
-                    Image(systemName: "play.circle") //(systemName: soundService.isPlaying ? "stop.circle" : "play.circle")
-                        .resizable()
-                        .foregroundColor(Color("colors/action"))
-                        .frame(width: 30, height: 30)
-                        .onTapGesture {
-                            // soundService.speach(language: language, text: example.example)
-                        }
-                }
+        CardView({
+            Content {
+                // Ensure that the image is not stretched to wide and to big
+                Image("for-children/\(item.imageName)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: 300)
+                    .padding(20)
             }
-            .padding(10)
-            .frame(maxWidth: .infinity)
-            .background(Color("colors/yellow"))
             
-        }
-        .background(.white)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        .padding()
-        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.04), radius: 38, x: 0, y: 19)
-        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.04), radius: 12, x: 0, y: 15)
+            Footer {
+                ForChildrenRowView(translation: item.translationFrom, transcription: item.transcriptionFrom, language: Languages.cs)
+                
+                ForChildrenRowView(translation: item.translationTo, transcription: item.transcriptionTo, language: Languages.uk)
+            }
+            
+        }, spacing: 0)
     }
 }
 
 struct ForChildrenItem_Previews: PreviewProvider {
+    static let soundService = SoundService()
+    
     static var previews: some View {
-        ForChildrenItemView(item: ForKidsItem.example)
+        ForChildrenItemView(item: ForChildrenItem.example)
+            .environmentObject(soundService)
     }
 }
 

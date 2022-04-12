ce82e19933da06457e42aa247b53039aede27d97
Radek Pištělák <radek.pistelak@gmail.com>
2022-04-12 16:51:18 +0200

    Add a few more links and partner section to the menu

--- Shared/Menu/MenuView.swift
+++ Shared/Menu/MenuView.swift
@@ -7,53 +7,86 @@
 
 import SwiftUI
 
-
-
 struct MenuView: View {
+
     var body: some View {
         
         List {
-            Section {
-                Button("Web") {
-                    openUrl("https://movapp.cz")
-                }
-                
-                Button("Twitter") {
-                    openUrl("https://twitter.com/movappcz")
-                }
-                
-                Button("Instagram") {
-                    openUrl("https://www.instagram.com/movappcz/")
-                }
-            } header: {
-                Image("logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: .infinity)
-                
-                Text("O projektu")
-            }
-            
-            
-            Section {
-                Button("Chci vylepšit aplikaci") {
-                    openUrl("https://github.com/cesko-digital/movapp-apple")
-                }
-                
-                Button("Licence") {
-                    openUrl("https://github.com/cesko-digital/movapp-apple/blob/main/LICENSE")
-                }
-            } header: {
-                Text("O aplikaci")
-            } footer: {
-                Text("Ver: \(Bundle.main.appVersionLong) (\(Bundle.main.appBuild)) ")
-                    
-            }
+            projectSection
+            appSection
+            partnerSection
         }
-        
     }
     
-    func openUrl(_ urlString: String)  {
+    private var projectSection: some View {
+        Section {
+            
+            Button("Web") {
+                openUrl("https://movapp.cz")
+            }
+            
+            Button("Twitter") {
+                openUrl("https://twitter.com/movappcz")
+            }
+            
+            Button("Facebook") {
+                openUrl("https://www.facebook.com/movappcz")
+            }
+            
+            Button("Instagram") {
+                openUrl("https://www.instagram.com/movappcz/")
+            }
+            
+            Button("LinkedIn") {
+                openUrl("https://www.linkedin.com/company/movapp-cz/")
+            }
+            
+        } header: {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+            
+            Text("menu.section.about_project.header")
+        }
+    }
+    
+    private var appSection: some View {
+        Section {
+            
+            Button("menu.section.about_app.want_help") {
+                openUrl("https://github.com/cesko-digital/movapp-apple")
+            }
+            
+            Button("menu.section.about_app.license") {
+                openUrl("https://github.com/cesko-digital/movapp-apple/blob/main/LICENSE")
+            }
+            
+        } header: {
+            Text("menu.section.about_app.header")
+        } footer: {
+            Text("Ver: \(Bundle.main.appVersionLong) (\(Bundle.main.appBuild)) ")
+        }
+    }
+    
+    private var partnerSection: some View {
+        Section {
+            
+            Button("Stojíme za Ukrajinou") {
+                openUrl("https://www.stojimezaukrajinou.cz/")
+            }
+            
+            Button("Umapa") {
+                openUrl("https://www.umapa.eu/")
+            }
+            
+        } header: {
+            Text("menu.section.partner_projects.header")
+        }
+    }
+    
+    private func openUrl(_ urlString: String)  {
+        
         guard let url = URL(string: urlString) else {
             return
         }
@@ -65,6 +98,7 @@ struct MenuView: View {
 }
 
 struct MenuView_Previews: PreviewProvider {
+    
     static var previews: some View {
         MenuView()
     }

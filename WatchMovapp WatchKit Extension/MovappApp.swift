d37b2cdfc8e038709b682a31268cff0246a91a4a
Darina Melis <darina05@email.cz>
2022-04-22 15:20:08 +0200

    FEAT: Root view added to Apple Watch

---
+++ WatchMovapp WatchKit Extension/MovappApp.swift
@@ -0,0 +1,19 @@
+//
+//  MovappApp.swift
+//  WatchMovapp WatchKit Extension
+//
+//  Created by Daryna Polevyk on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                RootContentView()
+            }
+        }
+    }
+}
---
+++ WatchMovapp WatchKit Extension/Root/RootItemView.swift
@@ -0,0 +1,32 @@
+//
+//  RootItemView.swift
+//  WatchMovapp WatchKit Extension
+//
+//  Created by Daryna Polevyk on 22.04.2022.
+//
+
+import SwiftUI
+
+struct RootItemView: View {
+    let imageName: String
+    let title: String
+    var body: some View {
+        HStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30, alignment: .leading)
+                .foregroundColor(Color("colors/yellow"))
+            
+            Spacer()
+            Text(title)
+        }
+        .padding()
+    }
+}
+
+struct RootItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootItemView(imageName: "icons/dictionary", title: "Dictionary")
+    }
+}

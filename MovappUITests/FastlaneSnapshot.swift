22d2b3d957db839c21508da68c1f65077ba4e46f
Martin Kluska <martin@kluska.cz>
2022-05-02 20:03:14 +0200

    Remove testLaunchPerformance from snapshot test case

--- MovappUITests/FastlaneSnapshot.swift
+++ MovappUITests/FastlaneSnapshot.swift
@@ -51,13 +51,4 @@ class FastlaneSnapshot: XCTestCase {
         snapshot("4 - Menu")
         
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }

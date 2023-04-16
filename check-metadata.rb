a22ac06c72229b3f64ffae91f86c84e1f130d4d1
Jakub Růžička <xruzax@gmail.com>
2023-04-16 13:36:20 +0200

    Precheck  (#112)
    * precheck on pullrequest
    
    * better configuration of precheck
    
    * Unresolved conflict between options: 'api_key' and 'username'
    
    * Disable include_in_app_purchases
    
    * renamed check step, prepared test
    
    * added offline precheck
    
    * Reverted SK keywords, added checking only on changes
    
    * fixed encoding, run build only when relevant files changes
    
    * added run on push to main

---
+++ check-metadata.rb
@@ -0,0 +1,32 @@
+require 'fileutils'
+
+def check_content_length(path, length)
+    data = File.open(path, :encoding => 'utf-8', &:read)
+    
+    if data.length > length
+        STDERR.puts("File: #{path} is too long. Expected max #{length} actual: #{data.length}")
+        exit(false)
+    end
+end
+
+def main
+    keywords_length = 100
+    subtitle_length = 30
+    
+    check_content_length("Fastlane/metadata/cs/keywords.txt", keywords_length)
+    check_content_length("Fastlane/metadata/cs/subtitle.txt", keywords_length)
+    
+    check_content_length("Fastlane/metadata/en-US/keywords.txt", keywords_length)
+    check_content_length("Fastlane/metadata/en-US/subtitle.txt", keywords_length)
+    
+    check_content_length("Fastlane/metadata/pl/keywords.txt", keywords_length)
+    check_content_length("Fastlane/metadata/pl/subtitle.txt", keywords_length)
+    
+    check_content_length("Fastlane/metadata/sk/keywords.txt", keywords_length)
+    check_content_length("Fastlane/metadata/sk/subtitle.txt", keywords_length)
+    
+    check_content_length("Fastlane/metadata/uk/keywords.txt", keywords_length)
+    check_content_length("Fastlane/metadata/uk/subtitle.txt", keywords_length)
+end
+
+main()

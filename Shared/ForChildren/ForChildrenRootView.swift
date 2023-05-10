dea9443cd938f69ec690afc7609d2a979937d599
Jakub Ruzicka <jakub.ruzicka@kosik.cz>
2023-05-10 13:22:10 +0200

    Memory game for all languages

--- Shared/ForChildren/ForChildrenRootView.swift
+++ Shared/ForChildren/ForChildrenRootView.swift
@@ -14,12 +14,7 @@ struct ForChildrenRootView: View {
     var body: some View {
         NavigationView {
             VStack {
-                // remove if when we will have support for all languages
-                if languageStore.currentLanguage == .csUk || languageStore.currentLanguage == .ukCs {
-                    router
-                } else {
-                    images
-                }
+               router
             }
             .navigationTitle(RootItems.for_children.title)
             .background(Color("colors/item"))
@@ -49,16 +44,17 @@ struct ForChildrenRootView: View {
                         .navigationTitle("for.children.words")
                 }
 
-                NavigationLink("for.children.stories") {
-                    storiesList
-                        .navigationTitle("for.children.stories")
+                if languageStore.currentLanguage == .csUk || languageStore.currentLanguage == .ukCs {
+                    NavigationLink("for.children.stories") {
+                        storiesList
+                            .navigationTitle("for.children.stories")
+                    }
                 }
-                #if DEBUG
+
                 NavigationLink("for.children.pexe") {
                     pexesoView
                         .navigationTitle("for.children.pexe")
                 }
-                #endif
             }
         }
         .padding(.top, 1)
